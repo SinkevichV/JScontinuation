c6c385a17c8b3aa0252770bb01c44c92dda16059
SinkevichV <sinkevichvasiliu@mail.ru>
2024-08-23 21:06:17 +0700

    les5

---
+++ les5/data.js
@@ -0,0 +1,199 @@
+const dataInfo = `[{
+  "results": [
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Hagen",
+        "last": "Kienzle"
+      },
+      "location": {
+        "street": {
+          "number": 2120,
+          "name": "Raiffeisenstraße"
+        },
+        "city": "Neu-Isenburg",
+        "state": "Brandenburg",
+        "country": "Germany",
+        "postcode": 32559,
+        "coordinates": {
+          "latitude": "-53.7013",
+          "longitude": "-117.0011"
+        },
+        "timezone": {
+          "offset": "+3:00",
+          "description": "Baghdad, Riyadh, Moscow, St. Petersburg"
+        }
+      },
+      "email": "hagen.kienzle@example.com",
+      "login": {
+        "uuid": "5683c934-d127-4227-9f31-a292870168e0",
+        "username": "bluebutterfly692",
+        "password": "vietnam",
+        "salt": "ZgiDklDg",
+        "md5": "6739b08a9e34c35b016a5b535ef32600",
+        "sha1": "bea7792c978e350e4b81740f6afe982b140e3c14",
+        "sha256": "cd7d0270daacb9d0f73399bb69f7c20df37501174c2189594f1c3090c04cd77c"
+      },
+      "dob": {
+        "date": "1952-04-21T14:28:11.379Z",
+        "age": 72
+      },
+      "registered": {
+        "date": "2014-03-15T02:48:06.170Z",
+        "age": 10
+      },
+      "phone": "0540-0458091",
+      "cell": "0172-4120230",
+      "id": {
+        "name": "SVNR",
+        "value": "14 210452 K 005"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/15.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/15.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/15.jpg"
+      },
+      "nat": "DE"
+    }
+  ],
+  "info": {
+    "seed": "8f2c725501d824f5",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+},
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Mrs",
+        "first": "Stella",
+        "last": "Romero"
+      },
+      "location": {
+        "street": {
+          "number": 6444,
+          "name": "Edwards Rd"
+        },
+        "city": "Buffalo",
+        "state": "New York",
+        "country": "United States",
+        "postcode": 35754,
+        "coordinates": {
+          "latitude": "83.1796",
+          "longitude": "88.9841"
+        },
+        "timezone": {
+          "offset": "+3:30",
+          "description": "Tehran"
+        }
+      },
+      "email": "stella.romero@example.com",
+      "login": {
+        "uuid": "ec99a31a-4979-420c-92ba-e41fab947b9d",
+        "username": "purpletiger358",
+        "password": "premium",
+        "salt": "KDKiaV1h",
+        "md5": "1441f538ce3e34607ba723ec834a4b94",
+        "sha1": "cea3594dc262abf78fdbe86f1d21b651bd796ceb",
+        "sha256": "72c69c2fe43309831cd061037227830432f6bb2fd2c1197f16e8460ae7edd7e1"
+      },
+      "dob": {
+        "date": "1948-12-16T05:46:25.233Z",
+        "age": 75
+      },
+      "registered": {
+        "date": "2021-05-24T00:45:51.586Z",
+        "age": 3
+      },
+      "phone": "(762) 576-8680",
+      "cell": "(668) 990-6348",
+      "id": {
+        "name": "SSN",
+        "value": "561-35-3432"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/women/62.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/62.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/62.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "c22c853103d6a59b",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+},
+{
+  "results": [
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Brendan",
+        "last": "Jackson"
+      },
+      "location": {
+        "street": {
+          "number": 6670,
+          "name": "Park Road"
+        },
+        "city": "Kilkenny",
+        "state": "Kilkenny",
+        "country": "Ireland",
+        "postcode": 18204,
+        "coordinates": {
+          "latitude": "0.9752",
+          "longitude": "-52.5736"
+        },
+        "timezone": {
+          "offset": "+5:00",
+          "description": "Ekaterinburg, Islamabad, Karachi, Tashkent"
+        }
+      },
+      "email": "brendan.jackson@example.com",
+      "login": {
+        "uuid": "17baa761-2ae2-49db-800c-6d1ef6e297cc",
+        "username": "beautifulrabbit485",
+        "password": "123asd",
+        "salt": "iN4jJ2SX",
+        "md5": "8d65d1b882b4728ad0b9f8200bdfa81d",
+        "sha1": "b45265bff5bb2282ddb31d89cec74f0b1f05f910",
+        "sha256": "09c9d712d27b9a6323d12d6173e1b66e8343341428a047bae1fd2173de1c1965"
+      },
+      "dob": {
+        "date": "1994-04-01T21:46:17.455Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2004-09-10T04:45:59.055Z",
+        "age": 19
+      },
+      "phone": "071-381-6477",
+      "cell": "081-839-3747",
+      "id": {
+        "name": "PPS",
+        "value": "9303193T"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/87.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/87.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/87.jpg"
+      },
+      "nat": "IE"
+    }
+  ],
+  "info": {
+    "seed": "f068178f32f38510",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}]`    
+// Api = https://randomuser.me/api/
